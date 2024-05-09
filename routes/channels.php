f81c1fae85423803e038e4f8791ab8862e5920ac
Matthew Johnson <matthew@ionline.com.au>
2024-05-10 08:32:51 +1000

    ..

--- routes/channels.php
+++ routes/channels.php
@@ -3,14 +3,14 @@
 use Illuminate\Support\Facades\Broadcast;
 
 // basically just serves as security for the channels sort of optional if you want to make it public
-Broadcast::channel('user.{userId}', function (User $user) {
-    return true;
-});
-
-Broadcast::channel('user_reset_channel', function () {
-    return true;
-});
-
-Broadcast::channel('start_user_clicks', function () {
-    return true;
-});
+//Broadcast::channel('user.{userId}', function (User $user) {
+//    return true;
+//});
+//
+//Broadcast::channel('user_reset_channel', function () {
+//    return true;
+//});
+//
+//Broadcast::channel('start_user_clicks', function () {
+//    return true;
+//});
